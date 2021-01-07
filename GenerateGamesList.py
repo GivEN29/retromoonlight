838cddd7025937fa3bde3a91824c014ab9f19b78
Joao Costa <joaopccosta@gmail.com>
2021-01-07 16:21:59 +0000

    Update GenerateGamesList.py

--- GenerateGamesList.py
+++ GenerateGamesList.py
@@ -8,12 +8,14 @@ import os
 import stat
 
 RefreshListScript = 'Refresh.sh'
-
 BashHeader = '#!/bin/bash\n'
 StreamStrings = {
-                 "1080p": 'moonlight stream -1080 -fps 60 -bitrate 50000 -codec h265 -app'
+                 "1080p": 'moonlight stream -1080 -fps 60 -bitrate 50000 -codec h265 -app',
+                 "4k": 'moonlight stream -4k -fps 60 -bitrate 50000 -codec h265 -app'
                 }
 roms_directory = '/home/pi/RetroPie/roms/moonlight/'
+sanitization_tokens = {":": ""," ": "_"}
+
 
 def clear_directory(folder_path):
     """
@@ -45,62 +47,60 @@ def read_games_list(file_path):
     return input_data
 
 
-def is_valid_listing(game_listing):
-    """
-    Check to see if a single line from the listings is a valid game name
-    :param game_listing: A single line of text from the Moonlight List command
-    :return: True if it is a valid listing, False otherwise
-    """
-    try:
-        int(game_listing.split(".")[0])
-        return True
-    except Exception as e:
-        return False
-
-
 def create_script(game_title, stream_string):
     """
     Creates the script to run a game title
     :param game_title: The name of the game to launch
     :param stream_string: The Moonlight command to launch
     """
-    script = '{}{}\"{}\"'.format(BashHeader, stream_string, game_title)
+    script = f"{BashHeader}{stream_string} \"{game_title}\""
     print('\nCreating a script for {}:'.format(game_title))
     print(script)
     return script
 
 
-def write_script(script, game_title):
+def write_script(script, resolution, game_title, dry_run=False):
     """
     Writes a script string to disk
     :param script: The string to be written as the script
+    :param resolution: The resolution at which the game will be streamed
     :param game_title: The game title to be used as the file name
     """
     try:
-        script_name = '{}{}.sh'.format(roms_directory, game_title.replace(":", ""))
-        print('Writing {} to disk...'.format(script_name))
-        f = open(script_name, "w+")
-        f.write(script)
-        f.close()
+        script_name = f"{roms_directory}{game_title}_{resolution}.sh"
+        print(f"Writing {script_name} to disk... ")
+        if not dry_run:
+            f = open(script_name, "w+")
+            f.write(script)
+            f.close()
 
-        st = os.stat(script_name)
-        os.chmod(script_name, st.st_mode | stat.S_IEXEC)
+            st = os.stat(script_name)
+            os.chmod(script_name, st.st_mode | stat.S_IEXEC)
     except Exception as write_exception:
         print(write_exception)
 
 
+def sanitize(game_name):
+    """
+    Removes unwanted characters from a game's name
+    :param game_name: Moonlight game name identifier
+    """
+    sanitized_game_name = game_name
+    for token in sanitization_tokens:
+        replacement = sanitization_tokens[token]
+        sanitized_game_name = sanitized_game_name.replace(token, replacement)
+    return sanitized_game_name
+
+
 try:
-    RawInputList = read_games_list(sys.argv[1])
+    moonlight_games = read_games_list(sys.argv[1])
+    clear_directory(roms_directory)
+
+    for gameListing in moonlight_games:
+        game_name = gameListing.strip().split('.')[1].strip()
+        sanitized_game_name = sanitize(game_name)
+        for resolution in StreamStrings:
+            launch_script = create_script(f"{game_name}", StreamStrings[resolution])
+            write_script(launch_script, resolution, sanitized_game_name, False)
 except Exception as e:
     exit(e)
-
-
-clear_directory(roms_directory)
-
-for gameListing in RawInputList:
-    parsedListing = gameListing.strip()
-    if is_valid_listing(parsedListing) is False:
-        continue
-    parsedListing = parsedListing.strip().split('.')[1].strip()
-    for resolution in StreamStrings:
-        write_script(create_script(f"{parsedListing}", StreamStrings[resolution]), parsedListing)
