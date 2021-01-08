d21b4af73a7ff208854ac215e3c0303d215ba292
Joao Costa <joaopccosta@gmail.com>
2021-01-08 09:08:32 +0000

    Update GenerateGamesList.py
    Create additional launch scripts

--- GenerateGamesList.py
+++ GenerateGamesList.py
@@ -9,12 +9,22 @@ import stat
 
 RefreshListScript = 'Refresh.sh'
 BashHeader = '#!/bin/bash\n'
+# https://github.com/irtimmer/moonlight-embedded/wiki/Usage
 StreamStrings = {
-                 "1080p": 'moonlight stream -1080 -fps 60 -bitrate 50000 -codec h265 -app',
-                 "4k": 'moonlight stream -4k -fps 60 -bitrate 50000 -codec h265 -app'
+                 "h264": 'moonlight stream -1080 -fps 60 -bitrate 50000 -codec h264 -app',
+                 "h265": 'moonlight stream -1080 -fps 60 -bitrate 50000 -codec h265 -app',
+                 "surround_264": 'moonlight stream -1080 -fps 60 -bitrate 50000 -codec h264 -surround -app',
+                 "surround_265": 'moonlight stream -1080 -fps 60 -bitrate 50000 -codec h265 -surround -app',
+                 "bitrate_20_h264": 'moonlight stream -1080 -fps 60 -bitrate 20000 -codec h264 -app',
+                 "bitrate_20_h265": 'moonlight stream -1080 -fps 60 -bitrate 20000 -codec h265 -app',
+                 "bitrate_20_surround_h264": 'moonlight stream -1080 -fps 60 -bitrate 20000 -codec h264 -surround -app',
+                 "bitrate_20_surround_h265": 'moonlight stream -1080 -fps 60 -bitrate 20000 -codec h265 -surround -app',
+                 # "4k": 'moonlight stream -4k -fps 60 -bitrate 50000 -codec h265 -app'
                 }
 roms_directory = '/home/pi/RetroPie/roms/moonlight/'
 sanitization_tokens = {":": ""," ": "_"}
+
+mock_games_list = ["1. Control", "2. Assassin's Creed Valhalla"]
 
 
 def clear_directory(folder_path):
@@ -47,19 +57,19 @@ def read_games_list(file_path):
     return input_data
 
 
-def create_script(game_title, stream_string):
+def create_script(game_title, stream_string, verbose=False):
     """
     Creates the script to run a game title
     :param game_title: The name of the game to launch
     :param stream_string: The Moonlight command to launch
     """
     script = f"{BashHeader}{stream_string} \"{game_title}\""
-    print('\nCreating a script for {}:'.format(game_title))
-    print(script)
+    if verbose:
+        print(f"Creating a script for {game_title}:\n{script}")
     return script
 
 
-def write_script(script, resolution, game_title, dry_run=False):
+def write_script(script, resolution, game_title, dry_run=False, verbose=False):
     """
     Writes a script string to disk
     :param script: The string to be written as the script
@@ -68,7 +78,8 @@ def write_script(script, resolution, game_title, dry_run=False):
     """
     try:
         script_name = f"{roms_directory}{game_title}_{resolution}.sh"
-        print(f"Writing {script_name} to disk... ")
+        if verbose:
+            print(f"Writing {script_name}... ")
         if not dry_run:
             f = open(script_name, "w+")
             f.write(script)
@@ -91,16 +102,24 @@ def sanitize(game_name):
         sanitized_game_name = sanitized_game_name.replace(token, replacement)
     return sanitized_game_name
 
+def run(dry_run, verbose):
+    try:
+        if not dry_run:
+            moonlight_games = read_games_list(sys.argv[3])
+            clear_directory(roms_directory)
+        else:
+            moonlight_games = mock_games_list
 
-try:
-    moonlight_games = read_games_list(sys.argv[1])
-    clear_directory(roms_directory)
+        for gameListing in moonlight_games:
+            game_name = gameListing.strip().split('.')[1].strip()
+            sanitized_game_name = sanitize(game_name)
+            print(f"Processing {game_name}...")
+            for resolution in StreamStrings:
+                launch_script = create_script(f"{game_name}", StreamStrings[resolution], verbose)
+                write_script(launch_script, resolution, sanitized_game_name, dry_run, verbose)
+    except Exception as e:
+        exit(e)
 
-    for gameListing in moonlight_games:
-        game_name = gameListing.strip().split('.')[1].strip()
-        sanitized_game_name = sanitize(game_name)
-        for resolution in StreamStrings:
-            launch_script = create_script(f"{game_name}", StreamStrings[resolution])
-            write_script(launch_script, resolution, sanitized_game_name, False)
-except Exception as e:
-    exit(e)
+dry_run = sys.argv[1]
+verbose = sys.argv[2]
+run(dry_run, verbose)
