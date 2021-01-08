c62dfb1396379fada855944149a4f41cf28796e5
Joao Costa <joaopccosta@gmail.com>
2021-01-08 09:13:35 +0000

    Update Refresh.sh

--- Scripts/Refresh.sh
+++ Scripts/Refresh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 moonlight list | tail -n +3 > gameslist.txt
-python3 ~/RetroPie/roms/moonlight/GenerateGamesList.py "./gameslist.txt"
+python3 False False ~/RetroPie/roms/moonlight/GenerateGamesList.py "./gameslist.txt"
