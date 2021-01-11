6608c967bbdfef059e430ef27feeb5184a7e94ab
Joao Costa <joaopccosta@gmail.com>
2021-01-11 09:28:43 +0000

    Update Refresh.sh

--- Scripts/Refresh.sh
+++ Scripts/Refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 moonlight list | tail -n +3 > gameslist.txt
 python3 ~/RetroPie/roms/moonlight/GenerateGamesList.py "./gameslist.txt"
-cp ~/retromoonlight/Scripts/force_quit.sh .
-chmod +x force_quit.sh
+cp ~/retromoonlight/Scripts/Force_Quit.sh .
+chmod +x Force_Quit.sh
