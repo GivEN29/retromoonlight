06c9d273eac6e5647ff368a153781cb0e7561171
Joao Costa <joaopccosta@gmail.com>
2021-01-22 15:42:29 +0000

    Update Install_Scripts.sh

--- Scripts/Install_Scripts.sh
+++ Scripts/Install_Scripts.sh
@@ -14,6 +14,16 @@ chmod a+x ./Scripts/Refresh.sh
 /bin/cp ./GenerateGamesList.py /home/pi/RetroPie/roms/moonlight/GenerateGamesList.py
 /bin/cp ./Scripts/Force_Quit.sh /home/pi/RetroPie/roms/moonlight/Force_Quit.sh
 
+echo "Input your network broadcast ip address (e.g. 192.168.1.255):"
+read $ip
+echo "Input your pc's network card mac address:"
+read $mac_addr
+
+sed -i "s/IP/$ip/g; s/MAC_ADDR/$mac_addr/g" WakeOnLan.sh
+chmod a+x ./Scripts/WakeOnLan.sh
+/bin/cp ./Scripts/WakeOnLan.sh /home/pi/RetroPie/roms/moonlight/WakeOnLan.sh
+echo -e "WakeOnLan script has been added to RetroPie\n"
+
 chmod 777 /home/pi/RetroPie/roms/moonlight
 
 echo -e "Refresh script has been added to RetroPie\n"
